07fd3c00286e4a1932a8ff389911bee9fc126ad9
Sarang Kate <sarangkate1605@gmail.com>
2024-09-29 10:59:22 +0530

    Phase 3 commit

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,29 +1,16 @@
 const express = require("express")
 const router = express.Router({mergeParams: true})
 const wrapAsync = require("../utils/wrapAsync.js")
-const Review = require("../models/review.js")
-const Listing = require("../models/listing.js")
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js")
+const reviewController = require("../controllers/reviewsController.js")
+
+// router.route method can be used to combine two or more
+// routes connecting to same route
 
 //Reviews Post Route
-router.post("/",isLoggedIn, validateReview, wrapAsync(async(req, res) => {
-    let listing = await Listing.findById(req.params.id)
-    let newReview = new Review(req.body.review)
-    listing.reviews.push(newReview)
-    newReview.author = req.user._id
-    await newReview.save()
-    await listing.save()
-    req.flash("success", "New Review created!")
-    res.redirect(`/listings/${listing._id}`)
-}))
+router.post("/",isLoggedIn, validateReview, wrapAsync(reviewController.postReview))
 
 //Reviews Delete Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(async(req, res) => {
-    let {id, reviewId} = req.params
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    req.flash("success", "Review deleted!")
-    res.redirect(`/listings/${id}`)
-}))
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(reviewController.deleteReview))
 
 module.exports = router
